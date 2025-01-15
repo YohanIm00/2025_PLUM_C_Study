ab0fd9831c9c67e5dd8ae426b814154e216247c0
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2025-01-15 20:32:48 +0900

    chore: add more commentaries

--- Review/w_01/4_fathm_ft.c
+++ Review/w_01/4_fathm_ft.c
@@ -1,13 +1,36 @@
 // fathm_ft.c -- converts 2 fathoms to feet
+/*
+Documentation
+:   The program begins with a comment identifying the file name and
+    the purpose of the program
+    -   This is helpful later when you browse through several files
+        or print them
+*/
 
 #include <stdio.h>
 int main()
 {
-    int feet, fathoms;
+    int feet, fathoms;  // Multiple Declarations
+    /*
+    Multiple Declarations
+    :   The program declares two variables instead of just one
+        in a single declaration statement
+    */
 
     fathoms = 2;
-    feet = 6 * fathoms;
-    printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+    feet = 6 * fathoms; // Multiplication
+    /*
+    Multiplication
+    :   The program makes a calculation as a basic numerical operation
+    :   In C, "*" is the symbol for multiplication
+    */
+    printf("There are %d feet in %d fathoms!\n", feet, fathoms);    // Printing multiple values
+    /*
+    Printing Values
+    :   if there are multiple substitutions, we can print two or more
+        values of each variable. Compiler will handle it procedurally
+    :   The result of expression can be printed as well
+    */
     printf("Yes, I said %d feet!\n", 6 * fathoms);
 
     return 0;
