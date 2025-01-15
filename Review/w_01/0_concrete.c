0fc4e724d070e44d5a67455b4c1de7c0545d4c9a
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2025-01-15 19:31:37 +0900

    feat: create concrete.c file

---
+++ Review/w_01/0_concrete.c
@@ -0,0 +1,16 @@
+/*
+Programming Mechanics
+1)  The exact programming steps depend on your programming environment
+2)  When you write a C code, you are going to store your writing
+    in a text file called "source code file"
+3)  Source code file: FILE_NAME.c
+    (e.g. practice.c, homework.c, text.c, ...)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+    printf("Concrete contains gravel and cement.\n");
+
+    return 0;
+}
