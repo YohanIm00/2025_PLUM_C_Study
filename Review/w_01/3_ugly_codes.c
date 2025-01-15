e810c22a0f1897b02ccbae78548e996cd6359a79
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2025-01-15 20:22:25 +0900

    feat: create ugly_code.c

---
+++ Review/w_01/3_ugly_codes.c
@@ -0,0 +1,20 @@
+/*
+Make Readable Codes
+1)  Choose meaningful variable names and use comments
+2)  Use blank lines to separate one conceptual section
+    of a function from another
+3)  Use one line per statement
+    -   Although you can place several statements on one line
+        or spread one statement over several lines...
+*/
+#include <stdio.h>
+
+int main(){ int four; four
+=
+4
+;
+printf(
+    "%d\n",
+four); return 0;}
+// Even though the program will be working well...
+// But it is truly hard to get it at once.
