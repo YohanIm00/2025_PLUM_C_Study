9a571b48e882eaddd967cd34ac200995829ba8be
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2025-03-17 17:56:23 +0900

    feat: create example w1-02

---
+++ Lecture Note/w01_02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    int year = 2025;
+    long long billion = 10000000;
+    float one = 1.0;
+    double pi = 3.141592653579;
+    char a = 'a';
+    bool isTrue = false;
+
+    return 0;
+}
