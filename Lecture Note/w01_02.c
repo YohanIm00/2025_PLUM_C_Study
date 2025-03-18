49f9360ea2dec0f305f52d22ef72f0bce89b1696
Yohan Im <119671375+YohanIm00@users.noreply.github.com>
2025-03-18 12:18:13 +0900

    chore: change code

--- Lecture Note/w01_02.c
+++ Lecture Note/w01_02.c
@@ -4,7 +4,7 @@
 int main()
 {
     int year = 2025;
-    long long billion = 10000000;
+    long long trillion = 1000000000000;
     float one = 1.0;
     double pi = 3.141592653579;
     char a = 'a';
